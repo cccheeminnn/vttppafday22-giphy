68035e2587a6cb665435b6b02904fb29b345f06d
CheeMin Wong <cheemin.wong1@outlook.com>
2022-04-05 11:04:54 +0800

    initial commit

---
+++ src/main/java/vttp2022/day22/giphy/GiphyApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.day22.giphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiphyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiphyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/day22/giphy/GiphyApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.day22.giphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiphyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
